a0ed6511b49b9b5bc5435722e1d748254654d86d
Gustavo Cardoso <gusstavocardoso@gmail.com>
2024-12-31 13:01:48 -0300

    first commit

---
+++ src/data-types/integer_type.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // Signed integer type
+    let x: i32 = -200;
+    let y: i32 = 200;
+
+    println!("x = {}", x);
+    println!("y = {}", y);
+}
---
+++ src/variables-mutability/mutable_variable.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // declare a mutable variable with value 1
+    let mut x = 1;
+    println!("Value of x = {}", x);
+
+    // change the value of variable x
+    x = 2;
+    println!("Updated value of x = {}", x);
+}
